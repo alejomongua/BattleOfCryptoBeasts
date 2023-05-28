e58946a332d57e99df17730b1670ff25b6b1de8f
Piyoju <piyoju@gmail.com>
2023-05-27 23:30:20 -0500

    Icon and style adjustments

--- crypto-beasts/src/components/market.js
+++ crypto-beasts/src/components/market.js
@@ -43,36 +43,40 @@ const Market = () => {
     }
 
     return (
-        <div className="App-content"> {
-            isLoading ?
-                <Spinner />
-            :
-            <div className='dashboard__list list_market'>
-                {offers.map((offer) => (
-                    <div key={offer.tokenId} className='dashboard__card'>
-                        <div className='dashboard__cardHeader'>
-                        <span className='marketHeader'>{`${offer.def.name}`}</span>
-                        <span className='dashboard__cardID'>{`Card ID: ${offer.cardId}`}</span>
-                        </div>
-                        <img src={offer.urlImg}
-                        alt={`Card ${offer.def.name}`}
-                        style={{maxHeight: '500px'}
-                        }/>
-                        <span className='marketHeader'>{`Rarity: ${offer.rarity === "1" ? 'Common' : offer.rarity === "2" ? 'Rare' : 'Legendary'}`}</span>
-                        
-                        <p className='market'>Price: <span className='market'>{`${(offer.offer[1] * 1e-18).toFixed(2)}CBC`}</span></p>
-                        {!isBuying ?
-                            <button className='dashboard__buy' onClick={() => onBuyToken(offer.tokenId.split('_')[0], offer.offer[1])}>{(allowedValue > parseInt(offer.offer[1])) ? 'Buy' : 'Approve Marketplace'}</button> 
-                            :
-                            <div className='loadBuy'>
-                                <Spinner />
-                                <span>Check Metamask!</span>
+        <div className="App-content">
+            {offers.length === 0 &&
+                <span>There are no available offers</span>
+            }
+            {offers.length > 0 && (
+                isLoading ?
+                    <Spinner />
+                :
+                <div className='dashboard__list list_market'>
+                    {offers.map((offer) => (
+                        <div key={offer.tokenId} className='dashboard__card'>
+                            <div className='dashboard__cardHeader'>
+                            <span className='marketHeader'>{`${offer.def.name}`}</span>
+                            <span className='dashboard__cardID'>{`Card ID: ${offer.cardId}`}</span>
                             </div>
-                        }
-                    </div>
-                ))}
-            </div>
-        } 
+                            <img src={offer.urlImg}
+                            alt={`Card ${offer.def.name}`}
+                            style={{maxHeight: '500px'}
+                            }/>
+                            <span className='marketHeader'>{`Rarity: ${offer.rarity === "1" ? 'Common' : offer.rarity === "2" ? 'Rare' : 'Legendary'}`}</span>
+                            
+                            <p className='market'>Price: <span className='market'>{`${(offer.offer[1] * 1e-18).toFixed(2)}CBC`}</span></p>
+                            {!isBuying ?
+                                <button className='dashboard__buy' onClick={() => onBuyToken(offer.tokenId.split('_')[0], offer.offer[1])}>{(allowedValue > parseInt(offer.offer[1])) ? 'Buy' : 'Approve Marketplace'}</button> 
+                                :
+                                <div className='loadBuy'>
+                                    <Spinner />
+                                    <span>Check Metamask!</span>
+                                </div>
+                            }
+                        </div>
+                    ))}
+                </div>)
+            }
         </div>
     );
 };
